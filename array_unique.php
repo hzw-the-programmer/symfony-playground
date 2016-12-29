94d7adaa16b5d79c85fb1d86a2a47e3e5d7ccc1c
hzhiwen <hzhiwen@ahong.com.cn>
2016-12-29 20:05:42 +0800

    array_unique

---
+++ array_unique.php
@@ -0,0 +1,19 @@
+<?php
+
+$a = ['a' => 'green', 'red', 'b' => 'green', 'blue', 'red'];
+var_dump(array_unique($a));
+
+$a = [10, '10', '9', 9, 10, '12'];
+var_dump(array_unique($a));
+
+class O {
+    private $str;
+    function __construct($str) {
+        $this->str = $str;
+    }
+    function __tostring() {
+        return $this->str;
+    }
+}
+$a = [new O('a'), new O('a'),new O('b'), new O('a'), new O('b'), new O('c')];
+var_dump(array_unique($a));
