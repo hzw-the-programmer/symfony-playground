109fc579d8be58728334c7655269741acc49836b
hzhiwen <hzhiwen@ahong.com.cn>
2017-01-03 20:25:49 +0800

    the right way to do fgetc

--- fgetc.php
+++ fgetc.php
@@ -7,6 +7,14 @@ file_put_contents($n, $c);
 
 $f = fopen($n, 'r');
 
+/*
 while(!feof($f)) {
     var_dump(fgetc($f));
 }
+*/
+while (($c = fgetc($f)) !== false) {
+    var_dump($c);
+}
+
+fclose($f);
+unlink($n);
