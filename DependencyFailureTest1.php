436d3a871744ed6931c3fd1a1a95f99e5430b736
hzhiwen <hzhiwen@ahong.com.cn>
2017-01-03 16:44:14 +0800

    test method execution order.

---
+++ DependencyFailureTest1.php
@@ -0,0 +1,15 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class DependencyFailureTest extends TestCase {
+    /**
+     * @depends testOne
+     */
+    public function testTwo() {
+    }
+
+    public function testOne() {
+        $this->assertTrue(true);
+    }
+}
