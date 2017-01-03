e78badbae4fb3663d5af8a4c5ec0be28baec8786
hzhiwen <hzhiwen@ahong.com.cn>
2017-01-03 16:37:59 +0800

    DependencyFailureTest

---
+++ DependencyFailureTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class DependencyFailureTest extends TestCase {
+    public function testOne() {
+        $this->assertTrue(false);
+    }
+
+    /**
+     * @depends testOne
+     */
+    public function testTwo() {
+    }
+}
