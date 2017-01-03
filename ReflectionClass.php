2b20b8d64e08c9e71e70be4a2bb492aa895fad99
hzhiwen <hzhiwen@ahong.com.cn>
2017-01-03 11:17:35 +0800

    ReflectionClass isCloneable.

--- ReflectionClass.php
+++ ReflectionClass.php
@@ -15,3 +15,56 @@ print_r($r->getProperties());
 print_r($r->getInterfaces());
 print_r($r->getInterfaceNames());
 var_dump($r->getParentClass());
+
+class A {
+}
+
+class B {
+    private function __clone() {
+    }
+}
+
+class C {
+    protected function __clone() {
+    }
+}
+
+class D {
+    public function __clone() {
+    }
+}
+
+class E {
+    function __clone() {
+    }
+}
+
+$r = new ReflectionClass('A');
+var_dump($r->isCloneable());
+//$m = $r->getMethod('__clone');
+//var_dump(Reflection::getModifierNames($m->getModifiers()));
+echo "\n";
+
+$r = new ReflectionClass('B');
+var_dump($r->isCloneable());
+$m = $r->getMethod('__clone');
+var_dump(Reflection::getModifierNames($m->getModifiers()));
+echo "\n";
+
+$r = new ReflectionClass('C');
+var_dump($r->isCloneable());
+$m = $r->getMethod('__clone');
+var_dump(Reflection::getModifierNames($m->getModifiers()));
+echo "\n";
+
+$r = new ReflectionClass('D');
+var_dump($r->isCloneable());
+$m = $r->getMethod('__clone');
+var_dump(Reflection::getModifierNames($m->getModifiers()));
+echo "\n";
+
+$r = new ReflectionClass('E');
+var_dump($r->isCloneable());
+$m = $r->getMethod('__clone');
+var_dump(Reflection::getModifierNames($m->getModifiers()));
+echo "\n";
