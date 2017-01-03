72293f362468ed11ffbfe48fd9bca2fe244478c9
hzhiwen <hzhiwen@ahong.com.cn>
2017-01-03 20:42:46 +0800

    ExceptionTest

---
+++ ExceptionTest.php
@@ -0,0 +1,8 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class ExceptionTest extends TestCase {
+    public function testException() {
+        $this->expectException(InvalidArgumentException::class);
+    }
+}
