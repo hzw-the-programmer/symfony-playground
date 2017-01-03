71b7676bc667cec79d6adc666d59ccfe0888eecd
hzhiwen <hzhiwen@ahong.com.cn>
2017-01-03 20:44:13 +0800

    use @expectedException annotation.

--- ExceptionTest.php
+++ ExceptionTest.php
@@ -2,7 +2,9 @@
 use PHPUnit\Framework\TestCase;
 
 class ExceptionTest extends TestCase {
+    /**
+     * @expectedException InvalidArgumentException
+     */
     public function testException() {
-        $this->expectException(InvalidArgumentException::class);
     }
 }
