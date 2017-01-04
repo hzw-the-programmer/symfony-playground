008802271c72a23d01582d7221cda892fa47c673
hzhiwen <hzhiwen@ahong.com.cn>
2017-01-04 08:52:50 +0800

    Suppress warning and make test work.

--- ErrorSuppressionTest.php
+++ ErrorSuppressionTest.php
@@ -4,7 +4,7 @@ use PHPUnit\Framework\TestCase;
 class ErrorSuppressionTest extends TestCase {
     public function testFileWriting() {
         $writer = new FileWriter();
-        $this->assertFalse($writer->write('/is-not-writeable/file', 'stuff'));
+        $this->assertFalse(@$writer->write('/is-not-writeable/file', 'stuff'));
     }
 }
 
