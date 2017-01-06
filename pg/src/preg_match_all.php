84f8a4366c0dae443ecec8d84405c9e65134f05c
hzhiwen <hzhiwen@ahong.com.cn>
2017-01-06 14:42:55 +0800

    preg_match_all

--- pg/src/preg_match_all.php
+++ pg/src/preg_match_all.php
@@ -1,6 +1,20 @@
 <?php
 
 preg_match_all('/(?:(A)|(B))/', 'aaaaAaaaaBaaaaa', $matches, PREG_SET_ORDER);
-foreach ($matches as $match) {
-    var_dump($match);
-}
+var_dump($matches);
+
+echo "\n";
+preg_match_all('/(foo)(bar)(baz)/', 'foobarbazafoobarbazbfoobarbazc', $matches/*, PREG_PATTERN_ORDER*/);
+var_dump($matches);
+
+echo "\n";
+preg_match_all('/(foo)(bar)(baz)/', 'foobarbazafoobarbazbfoobarbazc', $matches, /*PREG_PATTERN_ORDER | */PREG_OFFSET_CAPTURE);
+var_dump($matches);
+
+echo "\n";
+preg_match_all('/(foo)(bar)(baz)/', 'foobarbazafoobarbazbfoobarbazc', $matches, PREG_SET_ORDER);
+var_dump($matches);
+
+echo "\n";
+preg_match_all('/(foo)(bar)(baz)/', 'foobarbazafoobarbazbfoobarbazc', $matches, PREG_SET_ORDER | PREG_OFFSET_CAPTURE);
+var_dump($matches);
