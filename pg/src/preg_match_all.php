7c5405a3b21fe1b2321c9d657f7b2a670e01c6dc
hzhiwen <hzhiwen@ahong.com.cn>
2017-01-06 15:27:42 +0800

    preg_match_all

--- pg/src/preg_match_all.php
+++ pg/src/preg_match_all.php
@@ -1,20 +1,25 @@
 <?php
 
-preg_match_all('/(?:(A)|(B))/', 'aaaaAaaaaBaaaaa', $matches, PREG_SET_ORDER);
+$r = preg_match_all('/(?:(A)|(B))/', 'aaaaAaaaaBaaaaa', $matches, PREG_SET_ORDER);
+var_dump($r);
 var_dump($matches);
 
 echo "\n";
-preg_match_all('/(foo)(bar)(baz)/', 'foobarbazafoobarbazbfoobarbazc', $matches/*, PREG_PATTERN_ORDER*/);
+$r = preg_match_all('/(foo)(bar)(baz)/', 'foobarbazafoobarbazbfoobarbazc', $matches/*, PREG_PATTERN_ORDER*/);
+var_dump($r);
 var_dump($matches);
 
 echo "\n";
-preg_match_all('/(foo)(bar)(baz)/', 'foobarbazafoobarbazbfoobarbazc', $matches, /*PREG_PATTERN_ORDER | */PREG_OFFSET_CAPTURE);
+$r = preg_match_all('/(foo)(bar)(baz)/', 'foobarbazafoobarbazbfoobarbazc', $matches, /*PREG_PATTERN_ORDER | */PREG_OFFSET_CAPTURE);
+var_dump($r);
 var_dump($matches);
 
 echo "\n";
-preg_match_all('/(foo)(bar)(baz)/', 'foobarbazafoobarbazbfoobarbazc', $matches, PREG_SET_ORDER);
+$r = preg_match_all('/(foo)(bar)(baz)/', 'foobarbazafoobarbazbfoobarbazc', $matches, PREG_SET_ORDER);
+var_dump($r);
 var_dump($matches);
 
 echo "\n";
-preg_match_all('/(foo)(bar)(baz)/', 'foobarbazafoobarbazbfoobarbazc', $matches, PREG_SET_ORDER | PREG_OFFSET_CAPTURE);
+$r = preg_match_all('/(foo)(bar)(baz)/', 'foobarbazafoobarbazbfoobarbazc', $matches, PREG_SET_ORDER | PREG_OFFSET_CAPTURE);
+var_dump($r);
 var_dump($matches);
