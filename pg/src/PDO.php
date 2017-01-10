0b322962e6929418f980b87c5ea2e2eb3ef71f92
hzhiwen <hzhiwen@ahong.com.cn>
2017-01-10 19:23:43 +0800

    PDO.

--- pg/src/PDO.php
+++ pg/src/PDO.php
@@ -1,7 +1,12 @@
 <?php
-$dbh = new PDO('mysql:host=localhost;dbname=cms1', 'cms1user', 'xxxooo', array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+$dsn = 'mysql:host=localhost;dbname=cms1;charset=utf8';
+//$dsn = 'mysql:host=localhost;dbname=cms1';
+$usr = 'cms1user';
+$pwd = 'xxxooo';
+$opt = array();
+//$opt = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'");
+$dbh = new PDO($dsn, $usr, $pwd, $opt);
 foreach($dbh->query('select catname from rt_category') as $row) {
-    //var_dump($row);
     echo $row[0] . "\n";
     //echo mb_detect_encoding($row[0]) . "\n";
 }
