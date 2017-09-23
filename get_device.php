b27d346b98c929ceb5055c8e9fcf570cc471b5f2
hzw <18676797056@163.com>
2017-09-23 17:03:33 +0800

    multi sort works for device.

--- get_device.php
+++ get_device.php
@@ -1,7 +1,29 @@
 <?php
 $page = $_POST['page'];
 $rows = $_POST['rows'];
+$sort = $_POST['sort'];
+$order = $_POST['order'];
+
+if (!$page) {
+  $page = 1;
+}
+if (!$rows) {
+  $rows = 10;
+}
 $offset = ($page - 1) * $rows;
+
+$orderby = '';
+if ($sort && $order) {
+  $sorts = explode(',', $sort);
+  $orders = explode(',', $order);
+  $len = count($sorts);
+  for ($i = 0; $i < $len; $i++) {
+    $orderby .= "$sorts[$i] $orders[$i]";
+    if ($i != $len - 1) {
+      $orderby .= ',';
+    }
+  }
+}
 
 try {
   $results = [];
@@ -11,7 +33,12 @@ try {
   $results['total'] = $sth->fetch()[0];
   // $sth = $dbh->prepare('SELECT * FROM device LIMIT ?, ?');
   // $sth->execute(array($offset, $rows));
-  $sth = $dbh->prepare("SELECT * FROM device LIMIT $offset, $rows");
+  $select = 'SELECT * FROM device';
+  if ($orderby) {
+    $select .= " ORDER BY $orderby";
+  }
+  $select .= " LIMIT $offset, $rows";
+  $sth = $dbh->prepare($select);
   $sth->execute();
   $results['rows'] = $sth->fetchAll(PDO::FETCH_ASSOC);
 
