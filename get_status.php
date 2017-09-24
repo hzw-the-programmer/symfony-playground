e6c7a4acbafe17410c0f7f371f50a601e2d1249d
hzw <18676797056@163.com>
2017-09-24 11:30:22 +0800

    status pagination works.

--- get_status.php
+++ get_status.php
@@ -1,12 +1,41 @@
 <?php
 $cid = $_POST['id'];
+$page = $_POST['page'];
+$rows = $_POST['rows'];
+
+if (!$page) {
+  $page = 1;
+}
+if (!$rows) {
+  $rows = 10;
+}
+$offset = ($page - 1) * $rows;
 
 try {
   $dbh = new PDO('mysql:host=127.0.0.1;dbname=iot0', 'hzw', '123456');
-  $sth = $dbh->prepare('SELECT * FROM status WHERE cid = ?');
-  $sth->execute(array($cid));
 
-  echo json_encode($sth->fetchAll(PDO::FETCH_ASSOC));
+  $select = 'SELECT COUNT(*) FROM status';
+  $params = [];
+  if ($cid) {
+    $select .= ' WHERE cid = ?';
+    array_push($params, $cid);
+  }
+  $sth = $dbh->prepare($select);
+  $sth->execute($params);
+  $result['total'] = $sth->fetch()[0];
+
+  $select = 'SELECT * FROM status';
+  $params = [];
+  if ($cid) {
+    $select .= ' WHERE cid = ?';
+    array_push($params, $cid);
+  }
+  $select .= " LIMIT $offset, $rows";
+  $sth = $dbh->prepare($select);
+  $sth->execute($params);
+  $result['rows'] = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+  echo json_encode($result);
 
   $sth = null;
   $dbh = null;
