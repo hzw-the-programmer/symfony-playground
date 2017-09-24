f246b0f1201a131986eab53870d6be558c57cf7e
hzw <18676797056@163.com>
2017-09-24 11:09:08 +0800

    channel single order works.

--- get_channel.php
+++ get_channel.php
@@ -2,6 +2,8 @@
 $did = $_POST['id'];
 $page = $_POST['page'];
 $rows = $_POST['rows'];
+$sort = $_POST['sort'];
+$order = $_POST['order'];
 
 if (!$page) {
   $page = 1;
@@ -10,6 +12,11 @@ if (!$rows) {
   $rows = 10;
 }
 $offset = ($page - 1) * $rows;
+
+$orderby = '';
+if ($sort && $order) {
+  $orderby = "$sort $order";
+}
 
 try {
   $dbh = new PDO('mysql:host=127.0.0.1;dbname=iot0', 'hzw', '123456');
@@ -28,6 +35,9 @@ try {
   if ($did) {
     $select .= ' WHERE did = ?';
     array_push($params, $did);
+  }
+  if ($orderby) {
+    $select .= " ORDER BY $orderby";
   }
   $select .= " LIMIT $offset, $rows";
   $sth = $dbh->prepare($select);
