4b326449926379d1886b881a0483b25e23dc9f0e
hzw <18676797056@163.com>
2017-09-24 11:00:05 +0800

    channel pagination works.

--- get_channel.php
+++ get_channel.php
@@ -1,12 +1,40 @@
 <?php
 $did = $_POST['id'];
+$page = $_POST['page'];
+$rows = $_POST['rows'];
+
+if (!$page) {
+  $page = 1;
+}
+if (!$rows) {
+  $rows = 10;
+}
+$offset = ($page - 1) * $rows;
 
 try {
   $dbh = new PDO('mysql:host=127.0.0.1;dbname=iot0', 'hzw', '123456');
-  $sth = $dbh->prepare('SELECT * FROM channel WHERE did = ?');
-  $sth->execute(array($did));
+  $select = 'SELECT COUNT(*) FROM channel';
+  $params = [];
+  if ($did) {
+    $select .= ' WHERE did = ?';
+    array_push($params, $did);
+  }
+  $sth = $dbh->prepare($select);
+  $sth->execute($params);
+  $result['total'] = $sth->fetch()[0];
 
-  echo json_encode($sth->fetchAll(PDO::FETCH_ASSOC));
+  $select = 'SELECT * FROM channel';
+  $params = [];
+  if ($did) {
+    $select .= ' WHERE did = ?';
+    array_push($params, $did);
+  }
+  $select .= " LIMIT $offset, $rows";
+  $sth = $dbh->prepare($select);
+  $sth->execute($params);
+  $result['rows'] = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+  echo json_encode($result);
 
   $sth = null;
   $dbh = null;
